a25a97cc62b8ebf1bef785de57d3fd4e778b444e
Ralf Schimmel <r.schimmel@gynzy.com>
2016-12-30 13:06:47 +0100

    Support for snapshots or appending

--- app/index.js
+++ app/index.js
@@ -5,9 +5,14 @@ let sqlToBq = new RDBtoBigQuery();
 var tablesToSend = process.env.TABLES.split(',');
 var result = Promise.resolve();
 tablesToSend.forEach((table) => {
-  result = result.then(() => {
-    return sqlToBq.exec(table);    
-  });
+  let tableProperties = table.split(':');
+  if (tableProperties.length === 2 && (tableProperties[1] === 'append' || tableProperties[1] === 'snapshot')) {
+    result = result.then(() => {
+      return sqlToBq.exec(tableProperties[0],tableProperties[1]);    
+    });
+  } else {
+    console.error('table option not formatted correct, use comma separated list of `tablename:append` or `tablename:snapshot`, got: ' + table);
+  }
 });
 result.then(function (result) {
   console.log('All good, result: ', result);
--- app/lib/RDBtoBigQuery.js
+++ app/lib/RDBtoBigQuery.js
@@ -4,6 +4,9 @@ let esMap = require('event-stream').map;
 
 let ndjson = require('ndjson');
 
+const LOAD_METHOD_SNAPSHOT = 'snapshot';
+const LOAD_METHOD_APPEND = 'append';
+
 module.exports = class MySQLtoBigQuery {
 
   constructor() {
@@ -26,15 +29,16 @@ module.exports = class MySQLtoBigQuery {
     });
 
     this.dataset = this.bq.dataset(process.env.BQ_DATASET);
+    this.snapshotDataset = this.bq.dataset(process.env.BQ_DATASET_SNAPSHOTS);
   }
 
-  exec(table) {
-    return this._syncTable(table);
+  exec(table, loadMethod) {
+    return this._syncTable(table, loadMethod);
   }
 
-  _syncTable(tableName) {
+  _syncTable(tableName, loadMethod) {
     return new Promise((resolve, reject) => {
-      this._createOrUpdateTable(tableName).then(fields => {
+      this._createOrUpdateTable(tableName, loadMethod).then(fields => {
         return this._insertRecords(fields, tableName, this.dataset, this.limit);
       }).then(_ => {
         resolve();
@@ -42,7 +46,7 @@ module.exports = class MySQLtoBigQuery {
     });
   }
 
-  _createOrUpdateTable(tableName) {
+  _createOrUpdateTable(tableName, loadMethod) {
     return new Promise((resolve, reject) => {
 
       this.connection.query(`DESC ${tableName};`, (error, rows, fields) => {
@@ -62,17 +66,50 @@ module.exports = class MySQLtoBigQuery {
         const table = this.dataset.table(tableName);
         table.exists().then((data) => {
           var exists = data[0];
-          if (exists) {
-            console.log(`Table ${tableName} exists in BigQuery, will use existing schema, beware of schema changes, not updating for now...`);
-            //TODO update schema is needed
-            resolve(json);
+          if (exists && loadMethod === LOAD_METHOD_APPEND) {
+            if (loadMethod === LOAD_METHOD_APPEND) {
+              console.log(`Table ${tableName} exists in BigQuery, will use existing schema, beware of schema changes, not updating for now...`);
+              resolve(json);
+            }
           } else {
-            this.dataset.createTable(tableName, options, (error) => {
-              if (error) return reject(error);
-              // Give BQ some time before we start streaming data
-              setTimeout(function () {
-                resolve(json);
-              }, 3000);
+            var result = Promise.resolve();
+            if (exists && loadMethod === LOAD_METHOD_SNAPSHOT) {
+              console.log(tableName +  ' - Method is snapshot; moving old table to timestamped snapshot in snapshot dataset');
+
+              const timestamp = new Date().toISOString().replace(/T/g, '').replace(/-/g, '').replace(/:/g, '').replace(/\./g, '');
+              const destTableName = tableName + '_' + timestamp;
+              const destTable = this.snapshotDataset.table(destTableName);
+              const srcTable = this.dataset.table(tableName);
+              let job;
+
+              // Copies the table contents into another table
+              result = result.then(() => {
+                return srcTable.copy(destTable)
+                  .then((results) => {
+                    job = results[0];
+                    console.log(`Copy Job ${job.id} started for table ${tableName} to ${destTableName}`);
+                    return job.promise();
+                  })
+                  .then((results) => {
+                    console.log(`Job ${job.id} completed.`);
+                    return results;
+                  });
+              });
+              result = result.then(() => {
+                return srcTable.delete()
+                  .then(() => {
+                    console.log(`Table ${srcTable.id} deleted, will re-create it with new snapshot.`);
+                  });
+              })
+            }
+            result.then(() => {
+              this.dataset.createTable(tableName, options, (error) => {
+                if (error) return reject(error);
+                // Give BQ some time before we start streaming data
+                setTimeout(function () {
+                  resolve(json);
+                }, 3000);
+              });
             });
           }
         });
@@ -94,19 +131,21 @@ module.exports = class MySQLtoBigQuery {
         lastId = rows[0][0].maxId !== null ? rows[0][0].maxId : 0;
         console.log(`Selecting new rows for ${tableName} from ${lastId} with a limit of ${limit}.`);
         let query = this.connection.query(`SELECT * FROM ${tableName} WHERE id > ${lastId} ORDER BY id ASC LIMIT ${limit};`);
-        query.stream({highWaterMark: 100})
-        .pipe(esMap((data, cb) => cb(null, this._fix(data, fields))))
-            .pipe(ndjson.serialize())
-            .pipe(writeStream)
-            .on('complete', function (job) {
-              job.on('error', console.log)
-                .on('complete', function (metadata) {
-                  console.log('job completed', metadata);
-                  console.log('Done in ', (new Date().getTime() - start) / 1000, 'seconds');
-                  resolve();
-                });
-            })
-            .on('error', reject);
+        query.stream({
+            highWaterMark: 100
+          })
+          .pipe(esMap((data, cb) => cb(null, this._fix(data, fields))))
+          .pipe(ndjson.serialize())
+          .pipe(writeStream)
+          .on('complete', function (job) {
+            job.on('error', console.log)
+              .on('complete', function (metadata) {
+                console.log('job completed', metadata);
+                console.log('Done in ', (new Date().getTime() - start) / 1000, 'seconds');
+                resolve();
+              });
+          })
+          .on('error', reject);
       });
     });
   }
